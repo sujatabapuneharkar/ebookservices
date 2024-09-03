9823c7ca25b2a687d83a90a70d6a6eaf87e5ce60
Rohit Kangade <rohitnkm23@gmail.com>
2024-09-03 22:09:50 +0530

    Responsive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,17 @@
+// app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 const routes: Routes = [
-
   { path: 'login', component: LoginComponent },
-  { path: 'navbar', component: NavbarComponent },
+  { path: 'navbar', component: NavbarComponent }, 
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, 
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-
- }
+export class AppRoutingModule {}
